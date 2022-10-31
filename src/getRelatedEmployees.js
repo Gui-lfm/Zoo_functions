ec45414cd713773b1fe12062e5ba05af3e762f53
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-31 17:25:41 -0300

    requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const result = [];
+  employees.forEach((employee) => {
+    if (employee.managers.includes(managerId)) {
+      result.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  });
+
+  return result;
 }
 
 module.exports = { isManager, getRelatedEmployees };
