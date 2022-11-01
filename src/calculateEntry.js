c1da7864783728b6e92441a154b16374facb0e8d
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-01 19:09:45 -0300

    requisito 8 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      adult += 1;
+    } else {
+      senior += 1;
+    }
+  });
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const EntryQtd = countEntrants(entrants);
+  const { child, adult, senior } = EntryQtd;
+  const totalChildPrice = child * 20.99;
+  const totalAdultPrice = adult * 49.99;
+  const totalSeniorPrice = senior * 24.99;
+
+  return totalChildPrice + totalAdultPrice + totalSeniorPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
