fdfd27b6c6c5cc025e3bda0876bb7da34ad0bc93
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-03 16:28:47 -0300

    requisito 5 concluida

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,12 +5,19 @@ const { species } = data;
 function countAnimals(animal) {
   if (animal === undefined) {
     const result = {};
-    species.forEach((specie) => {
-      result[specie.name] = specie.residents.length;
-    });
+    species.forEach((specie) => { result[specie.name] = specie.residents.length; });
     return result;
   }
-  // const { specie, sex } = animal;
+  if ('sex' in animal) {
+    let count = 0;
+    const specieResidents = species.find((specie) => specie.name === animal.specie).residents;
+    specieResidents.forEach((resident) => {
+      if (resident.sex === animal.sex) { count += 1; }
+    });
+    return count;
+  }
+
+  return species.find((specie) => specie.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
