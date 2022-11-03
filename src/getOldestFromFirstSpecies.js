b026ac5e2102e54aa8b13c68ba084b448159b479
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-03 14:16:39 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const selectedEmployee = employees.find((employee) => employee.id === id);
+  const firstResponsible = selectedEmployee.responsibleFor[0];
+  const specieResidents = species.find(
+    (specie) => specie.id === firstResponsible,
+  ).residents;
+
+  const oldest = specieResidents.sort((a, b) => b.age - a.age);
+  const { name, sex, age } = oldest[0];
+
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getAnimalslist = () => {
+  const list = [];
+
+  species.forEach((specie) => {
+    const { name } = specie;
+    list.push(name);
+  });
+  return list;
+};
+
+const fullSchedule = () => {
+//
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animals = getAnimalslist();
+
+  if (animals.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  // if(scheduleTarget === undefined){
+  //   return
+  // }
 }
 
 module.exports = getSchedule;
