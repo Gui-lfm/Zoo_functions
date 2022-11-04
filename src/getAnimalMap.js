7c73c3915790936f4c84e894a4e9920390565c21
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-03 21:28:15 -0300

    requisito 14 concluido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -31,12 +31,30 @@ const getAnimalNames = (animal) => {
   return names;
 };
 
-const mapWithNames = () => {
+const getNamesByGender = (animal, gender) => {
+  const currentAnimal = species.filter((specie) => specie.name === animal);
+  const residents = Object.values(currentAnimal[0].residents);
+  const namesByGender = residents.filter((resident) => resident.sex === gender);
+
+  return namesByGender.map((filteredAnimal) => filteredAnimal.name);
+};
+
+const mapWithNames = (sorted = false, sex = false) => {
   const map = {};
+  console.log(sex);
   locations.forEach((location) => {
-    map[location] = animalByLocation(location).map((animal) => ({
-      [animal]: getAnimalNames(animal),
-    }));
+    map[location] = animalByLocation(location).map((animal) => {
+      let names = getAnimalNames(animal);
+      if (sex) {
+        names = getNamesByGender(animal, sex);
+      }
+      if (sorted) {
+        names.sort();
+      }
+      const list = { [animal]: names };
+
+      return list;
+    });
   });
 
   return map;
@@ -46,12 +64,10 @@ function getAnimalMap(options) {
   if (options === undefined || !('includeNames' in options)) {
     return defaultMap();
   }
-  if ('includeNames' in options) {
-    return mapWithNames();
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) {
+    return mapWithNames(sorted, sex);
   }
 }
-
-const options = { includeNames: true, sorted: true };
-console.log(getAnimalMap(options));
 
 module.exports = getAnimalMap;
